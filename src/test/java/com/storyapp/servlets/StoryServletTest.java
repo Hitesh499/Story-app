9b42fb0bb2d841c8661690e1033d8b5f300c21b2
Hitesh499 <hiteshdarshan136@gmail.com>
2025-07-24 10:44:41 +0530

    Create StoryServletTest.java

---
+++ src/test/java/com/storyapp/servlets/StoryServletTest.java
@@ -0,0 +1,13 @@
+package com.storyapp.servlets;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StoryServletTest {
+
+    @Test
+    public void testDummy() {
+        // This is a simple test that always passes
+        assertTrue(true, "This test should always pass");
+    }
+}
